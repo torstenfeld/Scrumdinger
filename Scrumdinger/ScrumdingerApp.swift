d6c29e5c939bca9a39bfa9fc8379e9c74b9a9990
Torsten Feld <torsten@torsten-feld.de>
2022-11-06 11:38:13 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Feld, Torsten on 06.11.22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
